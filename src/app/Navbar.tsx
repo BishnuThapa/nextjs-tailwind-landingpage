a5a260dbc3fb8f5720381f586965ba0852cbc1fb
Bishnu <thapabishnu20@gmail.com>
2024-01-16 13:34:22 +0545

    add submenu and group functionality of tailwind css for hover

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -17,8 +17,16 @@ const Navbar = () => {
             <div className="menu-item">
               <span>Lineup</span>
             </div>
-            <div className="menu-item">
+            <div className="menu-item group">
               <span>Tickets</span>
+              <div className="hidden group-hover:block bg-pink-500 absolute top-full right-0 whitespace-nowrap rounded-b-md text-right">
+                <div className="p-4 font-bold hover:bg-white/5 hover:text-zinc-200 transition-colors ease-in-out cursor-pointer text-pink-200">
+                  <span>Single Day Ticket</span>
+                </div>
+                <div className="p-4 font-bold hover:bg-white/5 hover:text-zinc-200 transition-colors ease-in-out cursor-pointer text-pink-200">
+                  <span>7 Day Ticket</span>
+                </div>
+              </div>
             </div>
             <div className="menu-item">
               <span>Support</span>
