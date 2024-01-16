3b61429bb38d0db5f39e1e5483a0bfcdc0b395e2
Bishnu <thapabishnu20@gmail.com>
2024-01-16 12:52:23 +0545

    add quicksand google font to project

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
+
 const Navbar = () => {
   return (
     <>
-      <header className='text-zinc-200'>
+      <header className="text-zinc-200">
         <nav className="sticky flex top-0 bg-gradient-to-r from-rose-500 to-pink-500">
-          <div className='flex items-center p-2 gap-2'>
-                      <img src="./logo.png" width="50" alt='site logo' />
-                      <div className='font-bold text-2xl '> Bishnu <span className='text-sky-900'>Thapa</span>
-                      </div> 
+          <div className="flex items-center p-2 gap-2">
+            <img src="./logo.png" width="50" alt="site logo" />
+            <div className="font-bold text-2xl ">
+              {" "}
+              Bishnu <span className="text-sky-900">Thapa</span>
+            </div>
           </div>
         </nav>
       </header>
     </>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
